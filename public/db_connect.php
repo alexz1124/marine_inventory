7f24dd9d261fc6f896533c95f400a10fa81450b5
alexz1124 <alexz.waranyu@gmail.com>
2025-03-25 16:39:59 +0700

    remove docker

--- public/db_connect.php
+++ public/db_connect.php
@@ -1,16 +1,9 @@
 <?php
-// เรียกใช้ autoload
-require_once __DIR__ . '/../vendor/autoload.php';
-
-// ระบุโฟลเดอร์ที่มี .env
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
-// ข้อมูลการเชื่อมต่อจาก .env
-$servername = $_ENV['DB_HOST'];
-$username = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASS'];
-$dbname = $_ENV['DB_NAME'];
+// ข้อมูลการเชื่อมต่อ
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "inventory";
 
 // เชื่อมต่อฐานข้อมูล
 $conn = new mysqli($servername, $username, $password, $dbname);
